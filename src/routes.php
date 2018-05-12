22ee0460dd2674f706e64d15d74aba01cd0d2f61
Angel Vargas <angelvargas@outlook.es>
2018-05-12 15:43:21 +0100

    Added extra fallback route to catch any missing request and then pointing to the 404 page

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,9 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+//catch all missing request to the 404 page
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
