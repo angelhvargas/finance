e40422741356e7c26672bc454278bdb46462a9be
Angel Vargas <angelvargas@outlook.es>
2018-05-12 22:11:28 +0100

    added port to the db connection configuration

--- src/settings.php
+++ src/settings.php
@@ -20,6 +20,7 @@ return [
         'db' => [
             'driver' => 'mysql',
             'host' => 'localhost',
+            'port' => '3306',
             'database' => 'finance',
             'username' => 'root',
             'password' => '',
