8447529d878f3f1d2cdc700507bdd2357012e9cc
Angel Vargas <angelvargas@outlook.es>
2018-05-12 15:42:02 +0100

    Added csrf token protection

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,31 @@
 <?php
+
+use Psr7Middlewares\Middleware\TrailingSlash;
 // Application middleware
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+//register csrf 
+$app->add($container->get('csrf'));
+
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+
+
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));
+});
